6e28269410e983a5e5820db52a81e31bcf2de753
diegodba9 <diegodba9@gmail.com>
2022-09-23 13:36:20 -0300

    first commit

---
+++ src/theme/index.ts
@@ -0,0 +1,25 @@
+export const THEME = {
+    COLORS: {
+        BACKGROUND:'F2F2F2',
+        TEXT: '#170126',
+        ALERT: '#73074D',
+
+
+        PRIMARY_900: '#280540',
+        PRIMARY_800: '#280540'
+    },
+
+    FONTS: {
+        REGULAR: 'Roboto_400Regular',
+        MEDIUM: 'Roboto_500Medium',
+        BOLD: 'Roboto_700Bold'
+
+    }
+};
+
+
+// cores do tema fonte das aguas
+// (#165865- verder -)
+// (#ffcc29- amarelo -)
+// (#ed3237- vermelho -)
+// (#13bab2- ciano fonte -)
